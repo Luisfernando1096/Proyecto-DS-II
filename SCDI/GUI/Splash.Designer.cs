ea360fa9064801c884f7b5403b70f54be8bf4925
fruiz <fruiz@LAPTOP-QBB4AB87>
2023-03-05 21:13:06 -0600

    Agregada la imagen al splash

--- SCDI/GUI/Splash.Designer.cs
+++ SCDI/GUI/Splash.Designer.cs
@@ -30,7 +30,10 @@ namespace SCDI.GUI
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Splash));
             this.cronometro = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // cronometro
@@ -38,15 +41,28 @@ namespace SCDI.GUI
             this.cronometro.Interval = 3000;
             this.cronometro.Tick += new System.EventHandler(this.cronometro_Tick);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-2, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(465, 272);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
             // Splash
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(515, 341);
+            this.ClientSize = new System.Drawing.Size(463, 270);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Splash";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Splash";
             this.Load += new System.EventHandler(this.Splash_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -54,5 +70,6 @@ namespace SCDI.GUI
         #endregion
 
         private System.Windows.Forms.Timer cronometro;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
