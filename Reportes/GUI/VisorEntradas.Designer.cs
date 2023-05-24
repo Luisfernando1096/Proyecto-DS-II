29bc1187d4c5c945bbd8e9fab52ee53ef811eb59
EdgarFuentes100 <edgarfuentes139@gmail.com>
2023-05-24 02:19:45 -0500

    Reportes Salidas, Entradas realizados

---
+++ Reportes/GUI/VisorEntradas.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace Reportes.GUI
+{
+    partial class VisorEntradas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.crvVisor = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.SuspendLayout();
+            // 
+            // crvVisor
+            // 
+            this.crvVisor.ActiveViewIndex = -1;
+            this.crvVisor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crvVisor.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crvVisor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crvVisor.Location = new System.Drawing.Point(0, 0);
+            this.crvVisor.Margin = new System.Windows.Forms.Padding(4);
+            this.crvVisor.Name = "crvVisor";
+            this.crvVisor.Size = new System.Drawing.Size(1049, 620);
+            this.crvVisor.TabIndex = 2;
+            this.crvVisor.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None;
+            // 
+            // VisorEntradas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1049, 620);
+            this.Controls.Add(this.crvVisor);
+            this.Name = "VisorEntradas";
+            this.Text = "VisorEntradas";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public CrystalDecisions.Windows.Forms.CrystalReportViewer crvVisor;
+    }
+}
